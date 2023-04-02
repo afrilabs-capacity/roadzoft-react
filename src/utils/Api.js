846a7722e4c34469aa2c4fbd84fa2de274e8ac79
afrilabs-capacity <factswarehouse360@gmail>
2023-04-02 09:59:42 +0100

    Changed demo link

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -2,5 +2,5 @@ import React from "react";
 
 // export const API_BASE = "/api";
 // export const API_BASE_UPLOADS = "/uploads";
-export const API_BASE = "https://roadzoftserver.xyz/api";
-export const API_BASE_UPLOADS = "https://roadzoftserver.xyz/uploads";
+export const API_BASE = "https://assets-fpamd.xyz/api";
+export const API_BASE_UPLOADS = "https://assets-fpamd.xyz/uploads";
